ba1bcecb55f7515d4d421cb86c6277ecf44fafc1
Gillett Hernandez <gillett.hernandez@gmail.com>
2022-08-16 23:21:30 -0700

    add check_nan, check_inf, and custom partial ord implementation

--- src/traits.rs
+++ src/traits.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
-use std::fmt::Debug;
 pub(crate) use std::ops::{Add, Div, Mul, Neg};
+use std::{cmp::Ordering, fmt::Debug};
 
 // differential forms of various measures
 pub trait Measure: Copy + Clone + Debug {}
@@ -78,6 +78,101 @@ pub trait Zero {
 //     const ZERO: Self = f32x4::splat(0.0);
 // }
 
+pub trait MyPartialOrd {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering>;
+}
+
+impl MyPartialOrd for f32 {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        PartialOrd::partial_cmp(self, other)
+    }
+}
+
+impl MyPartialOrd for f32x4 {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.gt(*other).all() {
+            Some(Ordering::Greater)
+        } else if self.eq(other) {
+            Some(Ordering::Equal)
+        } else if self.lt(*other).all() {
+            Some(Ordering::Less)
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub enum CheckResult {
+    None,
+    Some,
+    All,
+}
+
+impl CheckResult {
+    pub fn coerce(self, middle_destination: bool) -> bool {
+        match self {
+            CheckResult::All => true,
+            CheckResult::Some => middle_destination,
+            CheckResult::None => false,
+        }
+    }
+}
+
+pub trait CheckNAN {
+    fn check_nan(&self) -> CheckResult;
+}
+
+pub trait CheckInf {
+    fn check_inf(&self) -> CheckResult;
+}
+
+impl CheckNAN for f32 {
+    fn check_nan(&self) -> CheckResult {
+        if self.is_nan() {
+            CheckResult::All
+        } else {
+            CheckResult::None
+        }
+    }
+}
+
+impl CheckNAN for f32x4 {
+    fn check_nan(&self) -> CheckResult {
+        let mask = self.is_nan();
+        if mask.all() {
+            CheckResult::All
+        } else if mask.any() {
+            CheckResult::Some
+        } else {
+            CheckResult::None
+        }
+    }
+}
+
+impl CheckInf for f32 {
+    fn check_inf(&self) -> CheckResult {
+        if self.is_infinite() {
+            CheckResult::All
+        } else {
+            CheckResult::None
+        }
+    }
+}
+
+impl CheckInf for f32x4 {
+    fn check_inf(&self) -> CheckResult {
+        let mask = self.is_infinite();
+        if mask.all() {
+            CheckResult::All
+        } else if mask.any() {
+            CheckResult::Some
+        } else {
+            CheckResult::None
+        }
+    }
+}
+
 pub trait Field:
     Add<Output = Self>
     + Mul<Output = Self>
@@ -87,6 +182,7 @@ pub trait Field:
     + Clone
     + Copy
     + PartialEq
+    + MyPartialOrd
     + Debug
 {
     // trait bound to represent data types that can be integrated over.
